02335ac1ab240c3780459613cbb82adb62ba3a9a
Dahye Won <doall1102@gmail.com>
2024-09-11 05:05:48 +0900

    서버 데이터 받아온 PlanList

--- src/app/(AfterLogin)/posts/_component/PlanList.tsx
+++ src/app/(AfterLogin)/posts/_component/PlanList.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useAuth } from "@/app/context/AuthProvider";
 import { Plan } from "@/types/plan";
 import { Task } from "@/types/task";
@@ -24,20 +24,20 @@ export default function PlanList() {
   };
 
   const fetchTasksByPlanId = async (planId: number) => {
-    const { data } = await fetchWithAuth(`/api/task/${planId}`);
-    console.log("fetch! tasks!", data);
-    if (!data.length()) {
+    const res = await fetchWithAuth(`/api/task/${planId}`);
+    console.log("fetch! tasks!", res);
+    if (!res) {
       return;
     }
-    return data as Task[];
+    console.log("res", res)
+    return res as Task[];
   };
 
   const { data: planList, isLoading: isGetPlansLoading, error: getPlansError } = useQuery({queryKey: ['plansWithTasks'],
       queryFn: async () => {
       const plans = await fetchPlans();
       if(!plans) {
-        console.log("학습 계획이 없네유");
-        new Error("No plan");
+        console.log("No plan");
         return null;
       }
   
@@ -58,10 +58,6 @@ export default function PlanList() {
     }});
 
     if (isGetPlansLoading) return <div>Loading...</div>;
-    if (!planList) {
-      console.log("없다고?", planList);
-      return <div>No plan</div>;
-    }
   if (getPlansError) {
     console.log("error", getPlansError)
     return <div>Error loading plans</div>;
@@ -76,18 +72,13 @@ export default function PlanList() {
   
   return (
     <div className={styles.container}>
-      {planList ? planList.map((plan, planIndex) => (
-        <div key={planIndex}>
-          <h2>{plan.plan_name}</h2>
-          <h3>Tasks:</h3>
-          <ul>
-            {plan.tasks?.map((task, taskIndex) => (
-              <li key={`${planIndex}_${taskIndex}`}>
-                {task.title}
-              </li>
-            ))}
-          </ul>
-        </div>
+      {(!isGetPlansLoading && planList) ? planList.map((plan, planIndex) => (
+        <PlanItem
+        key={planIndex}
+          plan={plan}
+          isOpen={openPlan === planIndex}
+          togglePlan={() => togglePlan(planIndex)}
+          />
       )) : (<div>아직 학습 계획이 없어요!</div>)}
     </div>
   );
